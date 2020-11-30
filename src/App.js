731806f92ce251100f456e0ddf6555b1f860ee6a
Abhay Sarda <abhay.sarda@traivefinance.com>
2020-11-29 20:00:03 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          "Hijra Fauj Zindabag."
         </p>
         <a
           className="App-link"
